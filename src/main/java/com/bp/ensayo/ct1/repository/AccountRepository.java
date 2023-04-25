91cfd489c29745d538add116e5bafc36aeafa39f
Bladimir Minga <bsminga@pichincha.com>
2023-04-25 16:45:31 -0500

    Se soluciona error en Query

--- src/main/java/com/bp/ensayo/ct1/repository/AccountRepository.java
+++ src/main/java/com/bp/ensayo/ct1/repository/AccountRepository.java
@@ -2,11 +2,15 @@ package com.bp.ensayo.ct1.repository;
 
 import com.bp.ensayo.ct1.domain.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+    @Query("""
+            SELECT ae FROM com.bp.ensayo.ct1.domain.entity.AccountEntity ae WHERE ae.accountNumber = :accountNumber
+            """)
     Optional<AccountEntity> findByAccountNumber(String accountNumber);
 }
