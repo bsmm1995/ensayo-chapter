5ef67b0b21e65724229b16e51a785faa3384937a
Bladimir Minga <bsminga@pichincha.com>
2023-10-19 22:06:05 -0500

    Se agrega ejemplo de Order

---
+++ src/main/java/com/bp/ensayo/ct1/config/RequestResponseLoggingFilter.java
@@ -0,0 +1,30 @@
+package com.bp.ensayo.ct1.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(0)
+@Slf4j
+public class RequestResponseLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        log.warn("Logging Request  {} : {}", req.getMethod(), req.getRequestURI());
+        chain.doFilter(request, response);
+        log.warn("Logging Response :{}", res.getContentType());
+    }
+}
+
---
+++ src/main/java/com/bp/ensayo/ct1/config/TransactionFilter.java
@@ -0,0 +1,24 @@
+package com.bp.ensayo.ct1.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(2)
+@Slf4j
+public class TransactionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        log.info("Starting a transaction for req : {}", req.getRequestURI());
+        chain.doFilter(request, response);
+        log.info("Committing a transaction for req : {}", req.getRequestURI());
+    }
+}
+
